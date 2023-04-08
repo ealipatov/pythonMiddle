c52924e90faa9309e4b2e96e487f203d248fa391
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-04-08 22:19:51 +0300

    lesson_9 Создадим телеграм бота с игрой крестики-нолики

--- lessons/lesson_8.py
+++ lessons/lesson_8.py
@@ -7,7 +7,10 @@ bot = telebot.TeleBot("6010041191:AAGZVLrkK8MeJ2YVe1cMVk59_BnktnWj9xY")
 
 @bot.message_handler(content_types=['text'])
 def answer(message):
-    bot.send_message(message.chat.id, f'Ты написал мне: {message.text}')
+    if message.text == "Привет":
+        bot.send_message(message.chat.id, f'{message.text} Медвед!')
+    else:
+        bot.send_message(message.chat.id, f'Ты написал мне: {message.text}')
 
 
 bot.infinity_polling()
