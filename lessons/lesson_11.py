bc691292b4ff267a00b34382547acab50b0ec0ff
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-04-12 23:03:22 +0300

    lesson_11 Сохранили историю операций в файл, при запуске программы считываем данные из файла в память

--- lessons/lesson_11.py
+++ lessons/lesson_11.py
@@ -1,4 +1,7 @@
+import os.path
+
 ZNAK = ["+", "-", "/", "*"]
+OPERATION_HISTORY = "operation_history.txt"
 
 menu = {
     1: "Запустить калькулятор",
@@ -32,11 +35,27 @@ def calculate():
 
 
 def show_history_operation():
+    print("=" * 80)
     for cont, operation in enumerate(operation_log):
-        print(f"{cont + 1}: {operation}")
+        print(f"{cont + 1}: {operation}".replace("\n", ""))
+    print("=" * 80)
+
+
+def read_file(file_name):
+    with open(file_name) as file:
+        operation_log.extend(file.readlines())  # extend - добавить элементы одного списка в другой
+
+
+def write_to_file(file_name):
+    with open(OPERATION_HISTORY, "w") as file:
+        for operation in operation_log:
+            # file.write(f"{count}: {operation}")
+            file.write(operation.replace("\n", "") + "\n")
 
 
 def run():
+    if os.path.isfile(OPERATION_HISTORY):
+        read_file(OPERATION_HISTORY)
     while True:
         print(f"Меню: {menu}")
         selected_menu = int(input(f"Выберете пункт меню: "))
@@ -45,9 +64,11 @@ def run():
         elif selected_menu == 2:
             show_history_operation()
         elif selected_menu == 3:
+            write_to_file(OPERATION_HISTORY)
             print("Работа завершена.")
             break
         else:
             print("Ошибка ввода.")
+
 
 run()
