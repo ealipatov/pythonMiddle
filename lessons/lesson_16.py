5dabd7c28a1c5ac19247be28a15027e78f5050ca
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-04-16 10:19:15 +0300

    lesson_16 Заполнили таблицу вручную

--- lessons/lesson_16.py
+++ lessons/lesson_16.py
@@ -3,30 +3,71 @@ import datetime
 
 
 date_now = datetime.datetime.now()
-print(date_now)
-print(f"часов: {date_now.hour}")
-print(f"минут: {date_now.minute}")
-print(f"день: {date_now.day}")
-print(f"год: {date_now.year}")
+# print(date_now)
+# print(f"часов: {date_now.hour}")
+# print(f"минут: {date_now.minute}")
+# print(f"день: {date_now.day}")
+# print(f"год: {date_now.year}")
+#
+# print(date_now - datetime.timedelta(days=3))
 
-workbook = xlsxwriter.Workbook('test_files/test_exel.xlsx')
-worksheet = workbook.add_worksheet()
+students = {
+    "Петров": {
+        "Математика": [
+            (date_now - datetime.timedelta(days=1), 9),
+            (date_now - datetime.timedelta(days=3), 8),
+            (date_now - datetime.timedelta(days=5), 7)
+        ],
+        "Физика": [
+            (date_now, 7),
+            (date_now - datetime.timedelta(days=2), 8),
+            (date_now - datetime.timedelta(days=7), 9)
+        ]
+    },
+    "Сидорова": {
+        "Математика": [
+            (date_now - datetime.timedelta(days=1), 10),
+            (date_now - datetime.timedelta(days=3), 9),
+            (date_now - datetime.timedelta(days=5), 8)
+        ],
+        "Физика": [
+            (date_now, 8),
+            (date_now - datetime.timedelta(days=2), 9),
+            (date_now - datetime.timedelta(days=7), 10)
+        ]
+    }
+}
+# Создаем файл
+workbook = xlsxwriter.Workbook('test_files/students.xlsx')
 
-# Widen the first column to make the text clearer.
-worksheet.set_column('A:A', 20)
+# Создаем страницу (имя можно не указывать)
+worksheet = workbook.add_worksheet("Петров")
 
-# Add a bold format to use to highlight cells.
-bold = workbook.add_format({'bold': True})
+# Создадим переменную с форматом - жирный шрифт
+bold = workbook.add_format({"bold": True})
 
-# Write some simple text.
-worksheet.write('A1', 'Hello')
+# Сделаем записи в ячейки жирным шрифтом
+worksheet.write("A1", "Дата", bold)
+worksheet.write("B1", "Математика", bold)
+worksheet.write("C1", "Физика", bold)
 
-# Text with formatting.
-worksheet.write('A2', 'World', bold)
+worksheet.write("A2", str(date_now - datetime.timedelta(days=1))[:10])
+worksheet.write("A3", str(date_now - datetime.timedelta(days=2))[:10])
+worksheet.write("A4", str(date_now - datetime.timedelta(days=3))[:10])
+worksheet.write("A5", str(date_now - datetime.timedelta(days=5))[:10])
+worksheet.write("A6", str(date_now - datetime.timedelta(days=7))[:10])
 
-# Write some numbers, with row/column notation.
-worksheet.write(2, 0, 123)
-worksheet.write(3, 0, 123.456)
+worksheet.write("B2", 9)
+worksheet.write("B4", 8)
+worksheet.write("B5", 7)
+
+worksheet.write("C3", 8)
+worksheet.write("C6", 9)
+
+#
+# # Write some numbers, with row/column notation.
+# worksheet.write(2, 0, 123)
+# worksheet.write(3, 0, 123.456)
 
 # Insert an image.
 # worksheet.insert_image('B5', 'logo.png')
