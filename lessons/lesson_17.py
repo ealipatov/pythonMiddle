dae436b6ac50a70f2f1ec61cf9c7a3c29cca7207
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-04-16 17:16:01 +0300

    lesson_17 Сделали сохранение пользователей и их оценок в файл

--- lessons/lesson_17.py
+++ lessons/lesson_17.py
@@ -56,7 +56,7 @@ def check_password(login):
         return False
 
 
-def check_is_user_registered():
+def check_user_authentication():
     login = False
 
     while not login:
@@ -68,17 +68,82 @@ def check_is_user_registered():
     return login.lower()
 
 
+def save_data():
+    workbook = xlsxwriter.Workbook('test_files/students_mark.xlsx')
+    bold = workbook.add_format({"bold": True})
+
+    for user, marks in users_mark.items():
+        counter = 2
+        worksheet = workbook.add_worksheet(user)
+        worksheet.write("A1", "Дата", bold)
+        worksheet.write("B1", "Оценка", bold)
+
+        for mark in marks:
+            worksheet.write(f"A{counter}", str(mark[0])[:10] )
+            worksheet.write(f"B{counter}", mark[1])
+            counter += 1
+
+    workbook.close()
+
+
+def start_test(login):
+    right_answer = 0
+    for i in range(1, 6):
+        question, answer = generate_test()
+        try:
+            user_answer = int(input(f"Реши пример: {question} = "))
+            if user_answer == answer:
+                right_answer += 1
+                print("Правильный ответ")
+            else:
+                print(f"Ошибка. Правильный ответ: {answer}")
+        except Exception as error:
+            print(" Ошибка. Введенный ответ не целое число")
+    mark = right_answer * 2
+    users_mark[login] = users_mark.get(login, []) + [(date_now, mark)]
+
+    print(f"Правильных ответов: {right_answer}, оценка: {mark}")
+
+
+def print_dist(dist):
+    for key, value in dist.items():
+        print(key, ':')
+        for i in value:
+            print(i)
+
+
+def show_menu(user_access, login):
+    while True:
+        current_menu = MENU[user_access]
+        print(f"Меню пользователя {login}: {current_menu}")
+        chosen_item = int(input("Выберете пункт меню: "))
+        if user_access == STUDENT_ACCESS and chosen_item == 1:
+            start_test(login)
+        elif user_access == STUDENT_ACCESS and chosen_item == 2:
+            pass
+        elif user_access == ADMIN_ACCESS and chosen_item == 1:
+            pass
+        elif user_access == ADMIN_ACCESS and chosen_item == 2:
+            pass
+        elif chosen_item == 10:
+            if user_access == STUDENT_ACCESS:
+                save_data()
+                print("данные сохранены")
+            print("Работы программы завершена.")
+            return
+        else:
+            print("Выбран некорректный пункт меню")
+
+
 def run_program():
-    user_access = users[check_is_user_registered()][1]
+    login = check_user_authentication()
+    user_access = users[login][1]
     print(f"Ваш уровень доступа: {user_access}")
-    if user_access == ADMIN_ACCESS:
-        pass
-    elif user_access == STUDENT_ACCESS:
-        pass
+    show_menu(user_access, login)
 
 
 def generate_test():
-    znak = ["+", "-", "*", "/"][random.randint(0, 3)]
+    znak = ["+", "-", "*"][random.randint(0, 2)]
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
     question = f"{num1} {znak} {num2}"
@@ -86,5 +151,4 @@ def generate_test():
 
 
 run_program()
-
-print(users_mark)
+# print_dist(users_mark)
